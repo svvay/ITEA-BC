946c3b8b8e62766aff4f0fdaccfa936eed13489f
Sergey_Naumchick <sergey.naumchick@gmail.com>
2019-11-14 11:12:21 +0200

    Adding practice

---
+++ Sergey_Naumchick/2_data_types/Practise_01.py
@@ -0,0 +1,42 @@
+# Creating each type of variables
+
+Boolen = True
+Integer = 1
+Float = 3.3
+String = "Johny Dep"
+Tuple = ("John", "Ann")
+List = ["Bob", "Andy"]
+Set = set(["Kate", "Mike"])
+Dict = {"Rick": "Booba", "Helen": "Tom"}
+
+#Geating and typing each type of variables
+
+print(type(Boolen))
+print(type(Integer))
+print(type(Float))
+print(type(String))
+print(type(Tuple))
+print(type(List))
+print(type(Set))
+print(type(Dict))
+
+#Input and output data about person
+
+FName = input("Input Your first Name: ")
+LName = input("Input Your last Name: ")
+PNumber = input("Input Your phone number: ")
+Group = input("Input Your group : ")
+Char = input("Input a, b, c or d : ")
+print(FName + "\n" + LName + "\n" + PNumber + "\n" + Group + "\n" + Char)
+
+#Input variable
+a = 0
+
+#Some types of arithmetic operations
+a+=1
+a*=2
+a/=2
+a**=2
+
+#Output a
+print (a)
