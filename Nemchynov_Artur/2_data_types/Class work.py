8a8a869edbe635f1720b6361e55ed45358b513ee
Arenad <darkfootman@gmail.com>
2019-11-14 20:48:16 +0200

    delete git commit -m delete

---
+++ Nemchynov_Artur/2_data_types/Class work.py
@@ -0,0 +1,7 @@
+today = "Tuesday"
+my_visits = [ ]
+my_friends = ["John, Jack"]
+if today =="Monday"
+    if "Alise" not in my_friends
+        my_visits.append("Alise")
+        my_friend.append("Alise")
