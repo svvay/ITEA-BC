f3aa0dad3e18f682ae40433403ffb8dc5df24061
Arenad <darkfootman@gmail.com>
2019-11-19 15:56:46 +0200

    Homework3

--- Nemchynov_Artur/2_data_types/Class work.py
+++
@@ -1,7 +0,0 @@
-today = "Tuesday"
-my_visits = [ ]
-my_friends = ["John, Jack"]
-if today =="Monday"
-    if "Alise" not in my_friends
-        my_visits.append("Alise")
-        my_friend.append("Alise")
