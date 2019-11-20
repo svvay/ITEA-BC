8f6021fe578eb64c1722a1f612a18efe4d21de4f
kilvara <57716271+kilvara@users.noreply.github.com>
2019-11-20 12:32:40 +0200

    'Commit_homework_2'

--- Andrii_Ravlyk/2_data_types/hw_2.py
+++ Andrii_Ravlyk/2_data_types/hw_2.py
@@ -1,1 +1,45 @@
-print("t1")
+# HW
+
+# Create car:
+
+car = 'New_car'
+
+# Create variable price and increase price by 10 after each new input from user.
+price = 100
+
+# Use input for get:
+#   Brand
+brand = input("insert Brand= ")
+price +=  10
+#   Model
+model = input("insert Model= ")
+price +=  10
+# Color
+color = input("insert Color= ")
+price +=  10
+# Year
+year = int(input("insert Year= "))
+price +=  10
+# Engine volume
+engine_volume = input("insert engine_volume= ")
+price +=  10
+# Odometer
+odometer = int(input("insert odometer= "))
+price +=  10
+# Phone number
+phone_number = input("insert phone numbe= ")
+price +=  10
+
+# Multiply your price by 10 and divide by 100.
+price *= 10
+price /= 100
+
+# Decrease value of odometer and year.
+odometer -= 1
+year -=1
+
+# Output created car
+
+message = f"You car is {car} , price = {price}, brand = {brand}, model = {model}, color = {color}, year = {year}, " \
+          f"engine_volume = {engine_volume}, odometer = {odometer}, phone_number = {phone_number}  "
+print(message)
--- Andrii_Ravlyk/2_data_types/pr_2.py
+++ Andrii_Ravlyk/2_data_types/pr_2.py
@@ -1,1 +1,35 @@
-print("t2")
+# Practise
+
+# 1. Create each type variables
+v_int = 25
+v_str = "String"
+v_float = 10.2
+v_bool = True
+v_tuple = (1, 2)
+v_list = [1, 2]
+v_dictionary = {'a': 1, 'b': 2}
+# 2. Get variable type and output it by (print(type(variable_name)))
+print("v_int", type(v_int))
+print("v_str", type(v_str))
+print("v_float", type(v_float))
+print("v_bool", type(v_bool))
+print("v_tulip", type(v_tuple))
+print("v_list", type(v_list))
+print("v_dictionary", type(v_dictionary))
+
+# 3. Use input/output for get:
+#
+# * First Name
+# * Last Name
+# * Phone number
+# * Group
+# * a, b, c, d
+first_name = input("Type your first name: ")
+last_name = input("Type your last name: ")
+phone_number = input("Type your phone number: ")
+group = input("Type your group: ")
+mess1= f"Hi your first name  {first_name}, your last name {last_name}, phone number {phone_number}, group {group} "
+print(mess1)
+mess2="Hi your first name {}, your last name {}, phone number {}, group {}".format(first_name, last_name, phone_number, group)
+print(mess2)
+pass
