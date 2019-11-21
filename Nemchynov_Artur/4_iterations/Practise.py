25684238903d1eb972754229909fd02a250f4d88
Arenad <darkfootman@gmail.com>
2019-11-21 20:21:34 +0200

    Practise#4

--- Nemchynov_Artur/4_iterations/Practise.py
+++ Nemchynov_Artur/4_iterations/Practise.py
@@ -6,26 +6,3 @@ for item in my_list:
         continue
     print(item, type(item))
 pass
-
-numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) # Declaring the tuple
-count_odd = 0
-count_even = 0
-for x in numbers:
-        if not x % 2:
-            count_even+=1
-        else:
-            count_odd+=1
-print("Number of even numbers :",count_even)
-print("Number of odd numbers :",count_odd)
-pass
-
-lines = []
-while True:
-    l = input()
-    if l:
-        lines.append(l.upper())
-    else:
-        break
-for l in lines:
-    print(l)
-pass
