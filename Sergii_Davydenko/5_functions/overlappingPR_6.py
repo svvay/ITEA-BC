4eaf8c2fde43cd7d5a7cd9413b98bb8036450908
Svvay <svvay@ukr.net>
2019-11-28 23:36:44 +0200

    why

---
+++ Sergii_Davydenko/5_functions/overlappingPR_6.py
@@ -0,0 +1,25 @@
+# 6. Define a function `overlapping()` that takes two lists and
+# returns True if they have at least one member in common,
+# False otherwise. You may use your `is_member()` function,
+# or the in operator, but for the sake of the exercise,
+# you should (also) write it using two nested for-loops.
+
+# Its work
+
+first = input("Enter first lists: ").split()
+second = input("Enter second lists: ").split()
+
+
+def overlapping(first, second):
+    for f in first:
+        for s in second:
+            if f == s:
+                return True
+    return False
+
+print(overlapping(first, second))
+
+
+# Work for SET
+# count = fist.intersection(sec)
+# + count
