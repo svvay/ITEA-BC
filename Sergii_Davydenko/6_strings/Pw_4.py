7caba6cee3ae6fd0ddf19f3e9a94b1d419bf31c0
Svvay <svvay@ukr.net>
2019-11-28 19:08:09 +0200

    pr

---
+++ Sergii_Davydenko/6_strings/Pw_4.py
@@ -0,0 +1,9 @@
+#     `string = AV is largest Analytics community of India`
+# 4. Return first word from string.
+# result: `AV`
+
+# Done
+
+
+string = input('Enter something: ')
+print(string[:2])
