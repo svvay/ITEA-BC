532797b4236a64d150a3e562f0053e32e06d238a
Svvay <svvay@ukr.net>
2019-12-02 20:39:58 +0200

    Done3

---
+++ Sergii_Davydenko/6_strings/Pw/Pw_7.py
@@ -0,0 +1,10 @@
+#     `string = 'Amit 34-3456 12-05-2007, XYZ 56-4532 11-11-2011, ABC 67-8945 12-01-2009'`
+    # 7. Get date from string
+
+# Done
+
+
+string = 'Amit 34-3456 12-05-2007, XYZ 56-4532 11-11-2011, ABC 67-8945 12-01-2009'
+my_str = string.split( )
+print(my_str[2::3])
+
