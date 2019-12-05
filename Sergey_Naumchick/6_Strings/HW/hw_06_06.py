a382d189134ec9a66fa9b27ce24fbdec743ee5a3
RainEnigma <sergey.naumchick@gmail.com>
2019-12-05 17:15:22 +0200

    error corrections for lesson 6

--- Sergey_Naumchick/6_Strings/HW/hw_06_06.py
+++ Sergey_Naumchick/6_Strings/HW/hw_06_06.py
@@ -1,26 +1,12 @@
 # 6. Return list of domains from list of emails.
 
 li = ["Sky_fall@gmail.com", "def_get@i.ua", "Andriy_Zhopin@yahoo.com"]
-li = ['1221', 'sergey@gmail.com', 'koko@i.ua']
+lii = ['1221', 'sergey@gmail.com', 'koko@i.ua']
 
 
 def mail_return_domain(list_email):
-    domain_list = []
-
+    domail_list = []
     for word in list_email:
-        temp = False
-        temp_string = ""
-
-        for letter in word:
-
-            if temp == True:
-                temp_string += letter
-
-            if letter == "@":
-                temp = True
-
-        domain_list.append(temp_string)
-    print(domain_list)
-    return domain_list
-
-
+        if "@" in word:
+            domail_list.append(word.split("@")[1])
+    return domail_list
--- Sergey_Naumchick/6_Strings/HW/hw_6_02.py
+++ Sergey_Naumchick/6_Strings/HW/hw_6_02.py
@@ -1,8 +1,8 @@
 # 2. Write a function find_longest_word() that takes
 #    a list of words and returns the length of the longest one.
 
-def find_longest_word(my_list, x=0):
-    for i in my_list:
-        if len(i) > x:
-            x = len(i)
-    return x
+def find_longest_word(my_list, word_lenght=0):
+    for word in my_list:
+        if len(word) > word_lenght:
+            word_lenght = len(word)
+    return word_lenght
